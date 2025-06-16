d45e40ebb862a46e0b9e1a7a9e4109beb6760341
Akash Yadav <23bee006@nith.ac.in>
2025-06-16 18:35:23 +0000

    commit

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144456
+let accountEmail = "hitesh@google.com"
+var accountPassword ="12344"
+accountCity ="kanpur"
+let income;
+// accountID=2 not allowed
+
+accountEmail="akash"
+accountPassword="54321"
+accountCity="lucknow"
+/* Prefer not to use var because of issue in block scope and functional scope */
+
+console.table([accountEmail,accountId,accountPassword,accountCity,income])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+ "use strict";//treat all JS code as newer version
+
+ //alert(3 + 3)//we are using nodejs,not browser
+
+ console.log(3+3);
+  console.log("Akash")
+ 
+  let name ="akash"
+  let age =20
+  let Islog =false
+
+  //number=> 2 to power 53
+  //bigint
+  //string=>""
+  //boolean
+  //null=>standalone value
+  //undefiend=>
+    //symbol=> unique
+
+  //object
+
+  console.log(typeof undefined);//undefined
+  console.log(typeof null);//object
+  
+  
+
